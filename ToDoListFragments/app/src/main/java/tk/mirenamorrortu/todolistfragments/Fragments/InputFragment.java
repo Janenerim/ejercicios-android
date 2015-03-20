8d6109388bb6a7f36668e8ada30895efb41ade99
Janenerim <mirenamorrortu@gmail.com>
2015-03-20 19:34:58 +0100

    todo with fragments unfinished

---
+++ ToDoListFragments/app/src/main/java/tk/mirenamorrortu/todolistfragments/Fragments/InputFragment.java
@@ -0,0 +1,58 @@
+package tk.mirenamorrortu.todolistfragments.Fragments;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import tk.mirenamorrortu.todolistfragments.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class InputFragment extends Fragment {
+
+    public interface TODOItemListener{
+        public void addTodo(String todo);
+    }
+
+    private Button btnAdd;
+    private EditText todoText;
+
+    private TODOItemListener target;
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        try{
+            this.target = (TODOItemListener) activity;
+        }catch (ClassCastException ex){
+            throw new ClassCastException(activity.toString() + "must implement TODOItemListener interface");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View layout =  inflater.inflate(R.layout.fragment_input, container, false);
+
+        btnAdd = (Button) layout.findViewById(R.id.btnAdd);
+        todoText = (EditText) layout.findViewById(R.id.inputText);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String todo = todoText.getText().toString();
+                target.addTodo(todo);
+            }
+        });
+        return layout;
+    }
+}
---
+++ ToDoListFragments/app/src/main/java/tk/mirenamorrortu/todolistfragments/Fragments/ToDoListFragment.java
@@ -0,0 +1,10 @@
+package tk.mirenamorrortu.todolistfragments.Fragments;
+
+import android.app.ListFragment;
+
+/**
+ * A fragment representing a list of Items.
+ */
+public class ToDoListFragment extends ListFragment {
+
+}
